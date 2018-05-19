4fad3ac20edb4084b06a3bd27df67fa55c3274de
amadridlar <amadridlar@gmail.com>
2018-05-19 17:56:04 +0200

    modified jest.config.js to get test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  verbose: true,
+  "verbose": true,
+//uncomment the next line to get the coverage report
+//  "collectCoverage": true,
+  "collectCoverageFrom": [
+	    "app/*.{js,jsx}",
+	    "!**/node_modules/**",
+	    "!**/vendor/**"
+	    ],
+  "coverageDirectory":"test/reports/coverage",
 };
