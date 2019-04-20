87a120ad90585f5391aacf5917ec70f5ed1eee1e
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-04-20 16:38:07 +0200

    test(jest.config): change environment to node instead jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "verbose": true,
+	verbose: true,
+	testEnvironment: 'node',
 //uncomment the next line to get the coverage report
 //  "collectCoverage": true,
-  "collectCoverageFrom": [
-	    "app/*.{js,jsx}",
-	    "!**/node_modules/**",
-	    "!**/vendor/**"
+  collectCoverageFrom: [
+	    'app/*.{js,jsx}',
+	    '!**/node_modules/**',
+	    '!**/vendor/**'
 	    ],
-  "coverageDirectory":"test/reports/coverage",
+  coverageDirectory: 'test/reports/coverage',
 };
